41d1221d3a323c8b060d032db05da0ee8b99f25f
SamAdrn <samadrianko@gmail.com>
2021-11-17 13:25:16 -0500

    Update StackTest.java
    Added tests for clearing a Stack.

--- tests/stack/StackTest.java
+++ tests/stack/StackTest.java
@@ -50,14 +50,31 @@ class StackTest {
         Assertions.assertEquals(stack.size() - 1, stack.search(1));
     }
 
-    // Tests the general push and pop functionality of ArrayStack
+    // Tests the utility methods of ArrayStack
+
+    /**
+     * Tests clearing the {@link ArrayStack} object and test if it can be used
+     * again.
+     */
+    @Test
+    public void test4() {
+        // 1, 3, 9, 4, 5, 2, 10, 8, 6, 7
+        Stack<Integer> stack = TestData.arrayStack();
+        Assertions.assertFalse(stack.isEmpty());
+        stack.clear();
+        Assertions.assertTrue(stack.isEmpty());
+        stack.push(100);
+        Assertions.assertEquals(100, stack.peek());
+    }
+
+    // Tests the general push and pop functionality of LinkedStack
 
     /**
      * Tests the general functionality of pushing and popping elements in an
      * {@link LinkedStack} object.
      */
     @Test
-    public void test4() {
+    public void test5() {
         Stack<Integer> stack = TestData.linkedStack();
         List<Integer> list = Arrays.asList(1, 3, 9, 4, 5, 2, 10, 8, 6, 7);
         int stackSize = stack.size();
@@ -66,13 +83,13 @@ class StackTest {
         }
     }
 
-    // Tests the retrieval methods of ArrayStack
+    // Tests the retrieval methods of LinkedStack
 
     /**
      * Tests peeking elements at the top of an {@link LinkedStack}.
      */
     @Test
-    public void test5() {
+    public void test6() {
         // 1, 3, 9, 4, 5, 2, 10, 8, 6, 7
         Stack<Integer> stack = TestData.linkedStack();
         Assertions.assertEquals(7, stack.peek());
@@ -84,12 +101,29 @@ class StackTest {
      * Tests searching elements in an {@link LinkedStack} object.
      */
     @Test
-    public void test6() {
+    public void test7() {
         // 1, 3, 9, 4, 5, 2, 10, 8, 6, 7
         Stack<Integer> stack = TestData.linkedStack();
         Assertions.assertEquals(0, stack.search(7));
         Assertions.assertEquals(3, stack.search(10));
         Assertions.assertEquals(stack.size() - 1, stack.search(1));
+    }
+
+    // Tests the utility methods of LinkedStack
+
+    /**
+     * Tests clearing the {@link LinkedStack} object and test if it can be used
+     * again.
+     */
+    @Test
+    public void test8() {
+        // 1, 3, 9, 4, 5, 2, 10, 8, 6, 7
+        Stack<Integer> stack = TestData.linkedStack();
+        Assertions.assertFalse(stack.isEmpty());
+        stack.clear();
+        Assertions.assertTrue(stack.isEmpty());
+        stack.push(100);
+        Assertions.assertEquals(100, stack.peek());
     }
 
 }

127ebad545eb5d0782ee60d7d862cc945155175e
SamAdrn <samadrianko@gmail.com>
2021-11-16 19:18:44 -0500

    Update TestData.java
    Removed an unnecessary piece of test data.

--- tests/list/linkedlist/TestData.java
+++ tests/list/linkedlist/TestData.java
@@ -35,10 +35,34 @@ class TestData {
         return list;
     }
 
-    public static LinkedList<String> singlyList6() {
-        LinkedList<String> list = new SinglyLinkedList<>();
-        list.addAll(Arrays.asList("anteater", "snowy owl", "crocodile", "puppy",
-                "meerkat", "crocodile", "sheep", "puppy", "parrot", "sheep"));
+    public static LinkedList<Integer> doublyList1() {
+        LinkedList<Integer> list = new DoublyLinkedList<>();
+        list.addAll(Arrays.asList(1, 3, 9, 4, 5, 2, 10, 8, 6, 7));
+        return list;
+    }
+
+    public static LinkedList<Integer> doublyList2() {
+        LinkedList<Integer> list = new DoublyLinkedList<>();
+        list.addAll(Arrays.asList(0, -3, 12, -50, 41, 2, -10, 90, 23, 5));
+        return list;
+    }
+
+    public static LinkedList<Integer> doublyList3() {
+        LinkedList<Integer> list = new DoublyLinkedList<>();
+        list.addAll(Arrays.asList(0, 0, 0, 1, 1, 1, 1, 1, 2, 2));
+        return list;
+    }
+
+    public static LinkedList<Integer> doublyList4() {
+        LinkedList<Integer> list = new DoublyLinkedList<>();
+        list.addAll(Arrays.asList(0, 10, 9, 8, 10, 9, 0, 8, 8, 9));
+        return list;
+    }
+
+    public static LinkedList<String> doublyList5() {
+        LinkedList<String> list = new DoublyLinkedList<>();
+        list.addAll(Arrays.asList("bat", "elk", "horse", "ermine", "bear",
+                "wolverine", "cheetah", "baboon", "parrot", "eland"));
         return list;
     }
 
